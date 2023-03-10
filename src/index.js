9a26ace2ddd4911c18063712197d75a056f3a77f
calebchris000 <calebchris000@gmail.com>
2023-03-10 14:01:19 +0100

    Make changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./style.scss";
+import "./modules/menu.scss";
+import "./modules/email.scss";
+import "./modules/project.scss"
 require("./modules/profile.js");
+require("./modules/menu.js");
 
 const top = document.querySelector(".top");
 const wrapper = document.querySelector(".imageWrapper");
