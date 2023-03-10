a0db7abf8ae9e37e6710462aa17dec3da0ac1347
calebchris000 <calebchris000@gmail.com>
2023-03-10 16:54:11 +0100

    base

--- src/index.js
+++ src/index.js
@@ -7,9 +7,17 @@ require("./modules/menu.js");
 
 const top = document.querySelector(".top");
 const wrapper = document.querySelector(".imageWrapper");
+const navlinks = document.querySelector(".navlinks");
+const menu = document.querySelector(".menu");
 
 top.addEventListener("click", () => {
   wrapper.style.opacity === "1"
     ? (wrapper.style.opacity = "0")
     : (wrapper.style.opacity = "1");
 });
+
+top.addEventListener('click', () => {
+    navlinks.classList.contains('showMenu')? navlinks.classList.remove('showMenu'):0;
+    menu.classList.contains("click")
+    ? menu.classList.remove("click"):0
+})
